ac362dd7e047d0a8464f82eb68929449b2b08f1c
Boris Breuer <mail@borisbreuer.de>
2023-04-01 00:40:25 +0200

    interactive circle of fifth

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,25 +1,32 @@
-<script setup>
+<script setup lang="ts">
+import { start } from 'repl';
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useTheme } from 'vuetify'
 
 const theme = useTheme()
 const vColors = theme.global.current.value.colors
 
-const canvasEl = ref(null)
-const ctx = ref()
-const animationFrame = ref()
+const canvasEl = ref<HTMLCanvasElement | null>(null)
+const ctx = ref<CanvasRenderingContext2D | null>(null)
+const animationFrame = ref<number | null>(null)
+const isRunning = ref<boolean>(false)
 
-onMounted(() => {
-  init()
-})
+function startStopHandler() {
+  isRunning.value ? stop() : start();
+}
 
-onBeforeUnmount(() => {
+function start() {
+  if(canvasEl.value && !isRunning.value){
+    ctx.value = canvasEl.value.getContext('2d');
+    animationFrame.value = window.requestAnimationFrame(animate);
+    isRunning.value = true;
+  }
+}
+
+function stop() {
+  if(!animationFrame.value || !isRunning.value) return
   window.cancelAnimationFrame(animationFrame.value);
-})
-
-function init() {
-  ctx.value = canvasEl.value.getContext('2d');
-  animationFrame.value = window.requestAnimationFrame(animate)
+  isRunning.value = false;
 }
 
 let t = ref(0);
@@ -27,20 +34,22 @@ let x = ref(20);
 let y = ref(20);
 
 function animate() {
-  const WIDTH =  parseInt(canvasEl.value.width);
-  const HEIGHT = parseInt(canvasEl.value.height);
+  if(!ctx.value) return
+
+  const WIDTH = ctx.value.canvas.width;
+  const HEIGHT = ctx.value.canvas.height;
   
   const HALF_W = WIDTH/2; 
   const HALF_H = HEIGHT/2;
-  const PI = Math.PI; 
-  const TWO_PI = PI * 2;
+  const PI = Math.PI;
+  const TAU = PI * 2;
   const HALF_PI = PI/2;
 
   const r = 20
   const radius = HALF_W - r;
 
-  const sin = (s) => Math.sin(s);
-  const cos = (c) => Math.cos(c);
+  const sin = (s: number) => Math.sin(s);
+  const cos = (c: number) => Math.cos(c);
 
   ctx.value.clearRect(0, 0, WIDTH, HEIGHT);
 
@@ -49,13 +58,21 @@ function animate() {
   x.value = radius * cos((t.value) - HALF_PI) + HALF_W
   y.value = radius * sin((t.value) - HALF_PI) + HALF_H
 
-  ctx.value.arc(x.value, y.value, r, 0, TWO_PI);
+  ctx.value.arc(x.value, y.value, r, 0, TAU);
   ctx.value.fillStyle = vColors.deeporange;
   ctx.value.fill();
 
   t.value += 0.02;
   animationFrame.value = window.requestAnimationFrame(animate)
 }
+
+onMounted(() => {
+  start()
+})
+
+onBeforeUnmount(() => {
+  stop()
+})
 
 </script>
 
@@ -65,7 +82,8 @@ function animate() {
       <v-row class="d-flex align-center justify-center">
         <v-col cols="auto">
           <div>
-            <v-btn @click="() => t = 0">reset</v-btn>
+            <v-btn :disabled="!isRunning" @click="() => t = 0">reset</v-btn>
+            <v-btn @click="startStopHandler">{{ !isRunning ? 'start' : 'stop' }}</v-btn>
           </div>
           <canvas ref="canvasEl" width="400" height="400" class="ma-3"></canvas>
         </v-col>
--- src/components/CircleOfFifth.vue
+++ src/components/CircleOfFifth.vue
@@ -1,109 +1,207 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const major = ['C', 'G', 'D', 'A', 'E', 'B', 'Gb/<wbr>F#', 'Db', 'Ab', 'Eb', 'Bb', 'F']
+const minor = ['Am', 'Em', 'Bm', 'F#m', 'C#m', 'G#m', 'Ebm/<wbr>D#m', 'Bbm', 'Fm', 'Cm', 'Gm', 'Dm']
+
+const clicked = ref();
+const left = ref()
+const right = ref()
+const index = ref()
+const mm = ref([...major.map((item, i) => (
+  {
+    major: item,
+    minor: minor[i]
+  }
+))])
+
+
+
+function handler(event: MouseEvent) {
+  const el: HTMLDivElement = event.target as HTMLDivElement;
+  if(el.dataset.index) {
+    index.value = parseInt(el.dataset.index)
+    left.value = (index.value > 0) ? index.value - 1 : index.value - 1  + mm.value.length
+    right.value = (index.value < mm.value.length - 1) ? index.value + 1 : index.value + 1  - mm.value.length
+    clicked.value =  [mm.value[left.value], mm.value[index.value], mm.value[right.value]]
+  }
+}
+
+</script>
+
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto" class="pa-11 d-flex align-center justify-center">
-          <div class="circle">
-            <div v-for="(item, i) in major" :key="i + '_ma'" class="major">{{ item }}</div>
-          </div>
-          <div class="circle no-border">
-            <div v-for="(item, i) in minor" :key="i + '_mi'" class="minor">{{ item }}</div>
-          </div>
-        </v-col>
-      </v-row>
+  <v-container class="fill-height pb-0">
+    <v-responsive class="d-flex text-center fill-height pb-10">
+   
+    
+    <v-row  class="fill-height" >
+      <v-col cols="12" class="pa-5 d-flex align-center justify-center  fill-height">
+        <div class="circle">
+          <div 
+            v-for="(item, i) in major"
+            :key="i + '_ma'"
+            :data-index="i"
+            class="major"
+            :class="{ 'glow': (left === i || index === i || right === i) }"
+            v-html="item"
+            @click="handler"
+          ></div>
+        </div>
+        <div class="circle no-border">
+          <div
+            v-for="(item, i) in minor"
+            :key="i + '_mi'"
+            :data-index="i"
+            class="minor"
+            :class="{ 'glow': (left === i || index === i || right === i) }"
+            v-html="item"
+            @click="handler"
+          ></div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" >
+        <span>{{ clicked }}</span>
+      </v-col>
+    </v-row>
     </v-responsive>
   </v-container>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const major = ref([
-  'C', 'G', 'D', 'A', 'E', 'B', 'Gb/F#', 'Db', 'Ab', 'Eb', 'Bb', 'F'  
-])
-const minor = ref([
-  'a', 'e', 'b', 'f#', 'c#', 'g#', 'eb/d#', 'bb', 'f', 'c', 'g', 'd'  
-])
-</script>
-
 <style lang="scss">
 
 .circle {
-  --size-outer: 70vw;
+  $size-outer: 70;
+  --size-outer-w: #{$size-outer}vw;
+  --size-outer-h: #{$size-outer}vh;
+  aspect-ratio: 1;
   position: absolute;
   border: 1px solid rgb(var(--v-theme-deeporange));
   border-radius: 50%;
-  height: var(--size-outer);
-  aspect-ratio: 1;
+  margin: 3vw
+}
+
+@media screen and (orientation: portrait) {
+  .circle {
+    --responsive-size: var(--size-outer-w);
+    width: var(--responsive-size);
+    &.no-border {
+      --responsive-size: calc(var(--size-outer-w) - 11vw);
+      width: var(--responsive-size);
+    }
+    .major{
+      --size: 11vw;
+      --radius: calc(var(--responsive-size) / 2);
+      --center: calc(var(--responsive-size) / 2);
+    }
+    .minor {
+      --size: 10vw;
+      --radius: calc(var(--responsive-size) * 0.4);
+      --center: calc(var(--responsive-size) / 2);
+    }
+  }
+}
+
+@media screen and (orientation: landscape) {
+  .circle {
+    --responsive-size: var(--size-outer-h);
+    width: var(--responsive-size);
+    &.no-border {
+      --responsive-size: calc(var(--size-outer-h) - 11vh);
+      width: var(--responsive-size);
+    }
+    .major{
+      --size: 11vh;
+      --radius: calc(var(--responsive-size) / 2);
+      --center: calc(var(--responsive-size) / 2);
+    }
+    .minor {
+      --size: 10vh;
+      --radius: calc(var(--responsive-size) * 0.4);
+      --center: calc(var(--responsive-size) / 2);
+    }
+  }
+}
+
+.circle {
+  --time: 50ms;
 
   &.no-border {
     border: 1px solid transparent;
   }
-  
+
   .major {
-    --size: 60px;
-    --radius: calc(var(--size-outer) / 2);
-    --center: calc(var(--size-outer) / 2);
     --offset: calc( var(--size) / 2 );
     top: var(--center);
     left: var(--center);
     width: var(--size);
     position: absolute;
+    cursor: pointer;
+    font-size: 1rem;
+    line-height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
+    text-align: center;
     border-radius: 50%;
     background-color: rgb(var(--v-theme-deeporange));
     color: rgb(var(--v-theme-on-deeporange));
     aspect-ratio: 1;
+    // transition: width ease-in-out var(--time),  height ease-in-out var(--time);
     transform: 
       translate(
         calc(cos(var(--angle)) * var(--radius) - var(--offset)),
         calc(sin(var(--angle)) * var(--radius) - var(--offset))
-      )
+      );
+    &.glow {
+      border: 2px solid white;
+    }
   }
   
   @for $i from 1 through 12 {
-
     .major:nth-of-type(#{$i}) {
       --item: calc( 360deg / 12 );
       --start: calc(var(--item) * 4);
       --angle: #{$i} * var(--item) - var(--start);
     }
-
   }
 
   .minor {
-    --size: 50px;
-    --radius: calc(var(--size-outer) * 0.4);
-    --center: calc(var(--size-outer) / 2);
     --offset: calc( var(--size) / 2 );
+    font-size: 0.9rem;
+    line-height: 100%;
     top: var(--center);
     left: var(--center);
     width: var(--size);
     position: absolute;
+    cursor: pointer;
     display: flex;
     align-items: center;
     justify-content: center;
+    text-align: center;
     border-radius: 50%;
     background-color: rgb(var(--v-theme-orange));
     color: rgb(var(--v-theme-on-orange));
     aspect-ratio: 1;
+    // transition: width ease-in-out var(--time),  height ease-in-out var(--time);
     transform: 
       translate(
         calc(cos(var(--angle)) * var(--radius) - var(--offset)),
         calc(sin(var(--angle)) * var(--radius) - var(--offset))
-      )
+      );
+    &.glow {
+      border: 2px solid white;
+    }
   }
   
   @for $i from 1 through 12 {
-
     .minor:nth-of-type(#{$i}) {
       --item: calc( 360deg / 12 );
       --start: calc(var(--item) * 4);
       --angle: #{$i} * var(--item) - var(--start);
     }
-
   }
   
 }
